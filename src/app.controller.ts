c53e542f8c3652b14b97d3e93abbbf568f0fdbaf
xiaodye <bluewoods.lan@gmail.com>
2024-02-07 20:43:24 +0800

    feat: session add svgCaptcha

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as svgCaptcha from 'svg-captcha';
 
 @Controller()
 export class AppController {
     constructor(private readonly appService: AppService) {}
 
-    @Get('/hello')
+    @Get('hello')
     getHello(): string {
         return this.appService.getHello();
+    }
+
+    @Get('code')
+    @HttpCode(200)
+    createCode(@Req() req: any) {
+        const captcha = svgCaptcha.create({
+            size: 4,
+            fontSize: 50,
+            width: 100,
+            height: 34,
+            background: '#cc9966',
+        });
+
+        // req.type('image/svg+xml');
+        // req.send(captcha.data);
+
+        return {
+            code: 200,
+            msg: 'SUCCESS',
+            data: captcha.data,
+        };
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import * as session from 'express-session';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableVersioning({
         type: VersioningType.URI,
     });
+    app.use(session({ secret: 'xiaodye', rolling: true }));
     await app.listen(3114);
 }
 
